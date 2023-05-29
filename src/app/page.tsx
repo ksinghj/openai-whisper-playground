3f781e942382049629b1555e0302ae7e4d77ba87
ksinghj <kartarsjabanda@gmail.com>
2023-05-29 09:39:00 +0100

    feat(): completes POC and cleans up TS warnings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
 'use client'
 
-import { useState } from "react"
+const API_KEY = '' // <-- Add yours here!
+
+import { useState } from 'react'
 
 export default function Home() {
-  const [audioFile, setAudioFile] = useState()
+  const [audioFile, setAudioFile] = useState<Blob | string>()
+  const [result, setResult] = useState<string>()
 
   const transcribeAudio = async () => {
     const formData = new FormData()
 
     formData.append('model', 'whisper-1')
-    formData.append('file', audioFile)
+    formData.append('file', audioFile as Blob)
 
     try {
       const res = await fetch('https://api.openai.com/v1/audio/transcriptions', {
         headers: {
           Accept: 'application/json',
-          Authorization: 'Bearer API_KEY'
+          Authorization: `Bearer ${API_KEY}`
         },
         method: 'POST',
         body: formData
       })
-      const data = await res.json()
+      const data: { text: string } = await res.json()
+      setResult(data.text)
       console.log({ data })
     } catch (error) {
       console.log({ error })
@@ -30,7 +34,13 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Whisper API testing</h1>
-      <input type="file" onChange={(e) => setAudioFile(e.target.files[0])} />
+      <input
+        type='file'
+        onChange={e => {
+          if (e.target.files) setAudioFile(e.target.files[0])
+        }}
+      />
+      {result && <p>{result}</p>}
       <button onClick={transcribeAudio}>Test!</button>
     </main>
   )
